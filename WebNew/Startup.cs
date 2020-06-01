05e30eb699083a7dc49606305b40837443d6b80c
Administrator <Administrator@PC203>
2020-06-01 13:58:54 +0700

    Add project files.

---
+++ WebNew/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebNew.Startup))]
+namespace WebNew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
